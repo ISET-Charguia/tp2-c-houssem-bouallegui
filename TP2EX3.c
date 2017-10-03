2b7ee70e94bd45e26cd8acc9a9f227387db4c28a
houssem-bouallegui <32398198+houssem-bouallegui@users.noreply.github.com>
2017-10-03 21:55:37 +0200

    Create TP2EX3.c

---
+++ TP2EX3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+
+{
+
+	int a,b ;
+
+	float x ;
+
+	printf("donner la valeur de a :");
+
+	scanf("%d",&a);
+
+	printf("donner la valeur de b :");
+
+		scanf("%d",&b);
+
+
+
+		x=(-b)/(a);
+
+
+
+			printf("la solution de l'equation (ax+b) est : %3.f ",x);
+
+   return 0;
+
+}
