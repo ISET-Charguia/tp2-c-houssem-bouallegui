6b10ae70e7c66892b1d96ed9d7758c0c5e22fd64
houssem-bouallegui <32398198+houssem-bouallegui@users.noreply.github.com>
2017-10-04 10:27:57 +0100

    Update TP2EX1.c

--- TP2EX1.c
+++ TP2EX1.c
@@ -1,16 +1,25 @@
+/*
+ ============================================================================
+ Name        : TP2ex1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
 #include<stdio.h>
 int main ()
  {
    int x;
-   printf ("donner un entier");
+   printf ("donner un entier \n");
    scanf("%d",&x);
    if(x%3==0)
    {
-	  printf("cet entier est devisible par 3");
+	  printf("%d est devisible par 3",x);
    }
 	 else
 	 {
-		printf("cet entier n'est pas devisible par 3");
+		printf("%d n'est pas divésible par 3",x);
 	 }
   return 0;
 }
