9174c13be53653e00e15ed238bdbf2d02af6fe2b
houssem-bouallegui <32398198+houssem-bouallegui@users.noreply.github.com>
2017-10-04 10:46:55 +0100

    Update TP2EX7.c

--- TP2EX7.c
+++ TP2EX7.c
@@ -6,26 +6,26 @@ int main()
 
 	char c ;
 
-	printf("donner un caractére");
+	printf("donner un caractére \n");
 
 	scanf("%c",&c);
 
 	if((c>="A")&&(c<="Z"))
 
-	{	printf("c'est un caractere alphabetique majuscule");}
+	{	printf("c'est un caractere alphabetique majuscule \n");}
 
 	else if((c>="a")&&(c<="z"))
 
-		{printf("c'est caractere alphabetique minuscule");}
+		{printf("c'est caractere alphabetique minuscule \n");}
 
 
 
 	else if((c>="0")&&(c<="9"))
 
-	{	printf("c'est un chiffre");}
+	{	printf("c'est un chiffre \n");}
 
 			else
 
-				printf("c'est un symbole");
+				printf("c'est un symbole \n");
   return 0;
 }
